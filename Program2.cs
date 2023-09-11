6b93f0e0372094549ae42fe43e9ab3b97ee3b0a0
galyshko <144728311+galyshko@users.noreply.github.com>
2023-09-11 22:51:15 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace lab1._2
+{
+    internal class Program2
+    {
+        private static int length=10;
+
+        static void Main(string[] args)
+        {
+            var rndNumberGenerator = new RNGCryptoServiceProvider();
+            var randomNumber = new byte[length];
+            rndNumberGenerator.GetBytes(randomNumber);
+            Console.WriteLine(Convert.ToBase64String(randomNumber));
+            
+        }
+    }
+}
